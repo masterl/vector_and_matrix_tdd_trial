5612fca717d3c223546f8342388b5e2b95138620
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2018-08-21 19:55:22 -0700

    Removes using namespace std

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 
-using namespace std;
-
-int main(void)
+int main( void )
 {
+    using std::cout;
+    using std::endl;
+
     cout << "MAIN PRINCIPAL" << endl;
 
     return 0;
